974d5316023f808fd030abfd21cc5cce2d9ea9a8
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-07-18 22:15:30 -0400

    deploy

---
+++ send_mail.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = '5909664372d36b'
+    password = '98902ca74542fa'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'jackietristansantana@gmail.com'
+    receiver_email = 'jackietristansantana@gmail.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Lexus Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
